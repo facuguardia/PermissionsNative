69522b5bac818fd2315bc5b51893ffe0bda509f5
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-01-23 13:58:53 -0300

    Add new screens and dependencies

---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { HomeScreen } from '../screens/HomeScreen';
+import { PermissionsScreen } from '../screens/PermissionsScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+    </Stack.Navigator>
+  );
+}
